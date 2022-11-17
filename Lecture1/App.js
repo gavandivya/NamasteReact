b803c0165dbb28411efdad8e95620b5da747266f
Divya R. Gavandi <divya190399@gmail.com>
2022-11-17 14:46:46 +0530

    refactor:PR

--- Lecture1/App.js
+++
@@ -1,3 +0,0 @@
-const h3tag = React.createElement("h3", {}, "Hello Divya! I am displayed using React😄(External JS)");
-const getReactRoot1 = ReactDOM.createRoot(document.getElementById("reactRoot1"));
-getReactRoot1.render(h3tag);
