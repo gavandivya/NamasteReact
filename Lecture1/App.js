4a91c7761d8cdec749d94da4fe67145ad2de01ea
Divya R. Gavandi <divya190399@gmail.com>
2022-11-17 12:07:40 +0530

    docs:Updated README.md

--- Lecture1/App.js
+++ Lecture1/App.js
@@ -1,3 +1,3 @@
-const h3tag = React.createElement("h3", {}, "Hello Divya! I am are displayed using React😄(External JS)");
+const h3tag = React.createElement("h3", {}, "Hello Divya! I am displayed using React😄(External JS)");
 const getReactRoot1 = ReactDOM.createRoot(document.getElementById("reactRoot1"));
 getReactRoot1.render(h3tag);
