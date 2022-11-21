6a1b76f595d30583b79d1c2255b06593a2a60c31
Divya R. Gavandi <divya190399@gmail.com>
2022-11-21 14:42:47 +0530

    refactor:usingParcel

--- Lecture2/App.js
+++ Lecture2/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-// import ReactDOM from "react/client";
 
 const h1Tag = React.createElement("h1", {}, "Hello Divya!");
 const root = createRoot(document.getElementById('root'));
