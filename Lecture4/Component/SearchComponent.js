2835dbcbadfbf3ed556f228acd14995219f4af81
Divya R. Gavandi <divya190399@gmail.com>
2022-12-01 17:09:43 +0530

    feat:AddedSearchFunctionality

---
+++ Lecture4/Component/SearchComponent.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import data from '../data.json'
+
+const searchTeamdetails = (searchedText) => {
+    return data.filter((res) =>
+        res.name.toLocaleLowerCase().includes(searchedText.toLocaleLowerCase()) || res.designation.toLocaleLowerCase().includes(searchedText.toLocaleLowerCase())
+        || res.location.toLocaleLowerCase().includes(searchedText.toLocaleLowerCase()));
+}
+
+const SearchComponent = ({ setFilteredData }) => {
+    const [searchText, setSearchText] = useState("");
+    return (
+        <form className="form-inline m-3" onSubmit={(e) => {
+            e.preventDefault();
+            const filteredData = searchTeamdetails(searchText);
+            console.info(filteredData);
+            setFilteredData(filteredData);
+        }}>
+            <input
+                className="form-control mr-2 search"
+                type="search"
+                placeholder="Search"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+            />
+            <button className="btn my-2 my-0" type="submit">
+                <i className="fa fa-search"></i>
+            </button>
+        </form>
+
+    )
+}
+
+export default SearchComponent;
--- Lecture4/README.md
+++ Lecture4/README.md
@@ -8,7 +8,6 @@ Build your Team App
 - Add Photo, Name, Place, Company, Designation all info
 - Add CSS to your cards to make it look beautiful
 - Make your cards dynamic and data-driven
-
----
+- Add Search Functionality to filter out team member
 
 <!-- ![Image](https://github.com/gavandivya/NamasteReact/blob/sessionOne/Lecture2/assets/nav.png) -->
