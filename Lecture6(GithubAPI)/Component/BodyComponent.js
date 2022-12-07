a2c0f512cd165182016736cec5116e26354447a8
Divya R. Gavandi <divya190399@gmail.com>
2022-12-07 21:08:16 +0530

    fix:removedTryCatch

--- Lecture6(GithubAPI)/Component/BodyComponent.js
+++ Lecture6(GithubAPI)/Component/BodyComponent.js
@@ -15,27 +15,21 @@ const BodyComponent = () => {
     const githubUserNames = ["gavandivya", "akshaymarch7", "ap221882", "Bhallora", "aditifarkya", "it-abhishek2000", "deepak-kumar-dev", "soumyagangamwar", "shreerajjadeja", "nikitaj-57"]
 
     const getGithubApiData = async () => {
-        try {
-            let githubdata = await Promise.all(githubUserNames.map(async (userName) => {
-                const UserInfo = await fetch(`https://api.github.com/users/${userName}`);
-                return await UserInfo.json();
-            }));
-            sessionStorage.setItem('teamData', JSON.stringify(githubdata));
-            return githubdata;
-        }
-        catch (error) {
-            console.log(error);
-        }
+        let githubdata = await Promise.all(githubUserNames.map(async (userName) => {
+            const UserInfo = await fetch(`https://api.github.com/users/${userName}`);
+            return await UserInfo.json();
+        }));
+        sessionStorage.setItem('teamData', JSON.stringify(githubdata));
+        return githubdata;
     }
 
     useEffect(() => {
         if (!sessionStorage.getItem('teamData')) {
-            getGithubApiData().then((githubdata) => setDataFromAPI(githubdata))
-                .catch((err) => {
-                    console.log(err);
-                    setNoRecord(true);
-                })
-                .finally(() => setLoading(false));
+            getGithubApiData().then((githubdata) => {
+                setDataFromAPI(githubdata);
+            }).catch((err) => {
+                console.log(err);
+            }).finally(() => setLoading(false));
         }
         else {
             setDataFromAPI(JSON.parse(sessionStorage.getItem('teamData')));
