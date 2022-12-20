33d758cebbf9e205ae402643cfb40b41012e9a5a
Divya R. Gavandi <divya190399@gmail.com>
2022-12-20 12:36:35 +0530

    fix:linkedin

--- Lecture4/socialMediaIcon.js
+++ Lecture4/socialMediaIcon.js
@@ -1,6 +1,6 @@
 export const SocialMediaIcon = {
     discord: "https://img.icons8.com/color/48/null/discord-new-logo.png",
-    linkedin: "https://img.icons8.com/color/75/000000/linkedin.png",
+    linkedin: "https://img.icons8.com/color/75/null/linkedin.png",
     github: "https://img.icons8.com/ios-glyphs/75/null/github.png",
     instagram: "https://img.icons8.com/fluency/75/null/instagram-new.png",
     org: "https://img.icons8.com/plasticine/100/null/business-buildings.png",
